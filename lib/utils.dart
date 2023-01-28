eed16cf1d09740cab594cdd37c30578dda3a413c
Thomas Schüßler <v@atomar.de>
2023-01-28 11:14:33 +0100

    +sortBy for lists/maps

--- lib/utils.dart
+++ lib/utils.dart
@@ -18,3 +18,65 @@ void log(dynamic message) {
   //   d.log('$message $trace');
   // }
 }
+
+Map<K, V> sortByDeviceName<K, V>(Map<K, V> map, Map<String, String> deviceNames) {
+  return Map.fromEntries(({...map}).entries.toList()
+    ..sort(
+      // first sort by device name
+      (a, b) => deviceNames[a.key]!.compareTo(
+        deviceNames[b.key]!,
+      ),
+    ));
+}
+
+const _sortByDeviceName = sortByDeviceName;
+
+extension NameSorting<K, V> on Map<K, V> {
+  Map<K, V> sortByDeviceName(Map<String, String> deviceNames) {
+    return _sortByDeviceName(this, deviceNames);
+  }
+}
+
+List<T> sortByDeviceNameX<T>(List<T> list, Map<String, String> deviceNames) {
+  return List.from(
+    [...list]..sort(
+        // first sort by device name
+        (a, b) => deviceNames[a]!.compareTo(
+          deviceNames[b]!,
+        ),
+      ),
+  );
+}
+
+typedef CompareFn<A, B> = int Function(A a, B b);
+
+List<T> sortByList<T>(List<T> list, List<CompareFn> compareFns) {
+  for (final compareFn in compareFns) {
+    list.sort(compareFn);
+  }
+  return list;
+}
+
+const _sortByList = sortByList;
+
+extension ListMultipleSort<T> on List<T> {
+  List<T> sortByList(List<CompareFn> compareFns) {
+    return _sortByList(this, compareFns);
+  }
+}
+
+extension MapMultipleSort<K, V> on Map<K, V> {
+  Map<K, V> sortByList(List<CompareFn> compareFns) {
+    return Map.fromEntries((this).entries.toList()..sortByList(compareFns));
+  }
+}
+
+int compareName(String a, String b) {
+  return a.compareTo(b);
+}
+
+// var testList = [2, 3, 5, 6];
+// var newList = sortByList(testList, [
+//   (a, b) => a.compareTo(b),
+//   (a, b) => b.compareTo(a),
+// ]);

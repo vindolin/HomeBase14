b56b571c18f626a3352f6527f01e9f8a8f2d533e
Thomas Schüßler <v@atomar.de>
2023-03-01 09:39:30 +0100

    version freezed models for fast checkout

---
+++ lib/models/app_settings.freezed.dart
@@ -0,0 +1,275 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'app_settings.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+AppSettingsCls _$AppSettingsClsFromJson(Map<String, dynamic> json) {
+  return _AppSettingsCls.fromJson(json);
+}
+
+/// @nodoc
+mixin _$AppSettingsCls {
+  String get mqttUsername => throw _privateConstructorUsedError;
+  String get mqttPassword => throw _privateConstructorUsedError;
+  String get mqttAddress => throw _privateConstructorUsedError;
+  int get mqttPort => throw _privateConstructorUsedError;
+  User get user => throw _privateConstructorUsedError;
+  bool get valid => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $AppSettingsClsCopyWith<AppSettingsCls> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AppSettingsClsCopyWith<$Res> {
+  factory $AppSettingsClsCopyWith(
+          AppSettingsCls value, $Res Function(AppSettingsCls) then) =
+      _$AppSettingsClsCopyWithImpl<$Res, AppSettingsCls>;
+  @useResult
+  $Res call(
+      {String mqttUsername,
+      String mqttPassword,
+      String mqttAddress,
+      int mqttPort,
+      User user,
+      bool valid});
+}
+
+/// @nodoc
+class _$AppSettingsClsCopyWithImpl<$Res, $Val extends AppSettingsCls>
+    implements $AppSettingsClsCopyWith<$Res> {
+  _$AppSettingsClsCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? mqttUsername = null,
+    Object? mqttPassword = null,
+    Object? mqttAddress = null,
+    Object? mqttPort = null,
+    Object? user = null,
+    Object? valid = null,
+  }) {
+    return _then(_value.copyWith(
+      mqttUsername: null == mqttUsername
+          ? _value.mqttUsername
+          : mqttUsername // ignore: cast_nullable_to_non_nullable
+              as String,
+      mqttPassword: null == mqttPassword
+          ? _value.mqttPassword
+          : mqttPassword // ignore: cast_nullable_to_non_nullable
+              as String,
+      mqttAddress: null == mqttAddress
+          ? _value.mqttAddress
+          : mqttAddress // ignore: cast_nullable_to_non_nullable
+              as String,
+      mqttPort: null == mqttPort
+          ? _value.mqttPort
+          : mqttPort // ignore: cast_nullable_to_non_nullable
+              as int,
+      user: null == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as User,
+      valid: null == valid
+          ? _value.valid
+          : valid // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_AppSettingsClsCopyWith<$Res>
+    implements $AppSettingsClsCopyWith<$Res> {
+  factory _$$_AppSettingsClsCopyWith(
+          _$_AppSettingsCls value, $Res Function(_$_AppSettingsCls) then) =
+      __$$_AppSettingsClsCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String mqttUsername,
+      String mqttPassword,
+      String mqttAddress,
+      int mqttPort,
+      User user,
+      bool valid});
+}
+
+/// @nodoc
+class __$$_AppSettingsClsCopyWithImpl<$Res>
+    extends _$AppSettingsClsCopyWithImpl<$Res, _$_AppSettingsCls>
+    implements _$$_AppSettingsClsCopyWith<$Res> {
+  __$$_AppSettingsClsCopyWithImpl(
+      _$_AppSettingsCls _value, $Res Function(_$_AppSettingsCls) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? mqttUsername = null,
+    Object? mqttPassword = null,
+    Object? mqttAddress = null,
+    Object? mqttPort = null,
+    Object? user = null,
+    Object? valid = null,
+  }) {
+    return _then(_$_AppSettingsCls(
+      mqttUsername: null == mqttUsername
+          ? _value.mqttUsername
+          : mqttUsername // ignore: cast_nullable_to_non_nullable
+              as String,
+      mqttPassword: null == mqttPassword
+          ? _value.mqttPassword
+          : mqttPassword // ignore: cast_nullable_to_non_nullable
+              as String,
+      mqttAddress: null == mqttAddress
+          ? _value.mqttAddress
+          : mqttAddress // ignore: cast_nullable_to_non_nullable
+              as String,
+      mqttPort: null == mqttPort
+          ? _value.mqttPort
+          : mqttPort // ignore: cast_nullable_to_non_nullable
+              as int,
+      user: null == user
+          ? _value.user
+          : user // ignore: cast_nullable_to_non_nullable
+              as User,
+      valid: null == valid
+          ? _value.valid
+          : valid // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_AppSettingsCls
+    with DiagnosticableTreeMixin
+    implements _AppSettingsCls {
+  const _$_AppSettingsCls(
+      {required this.mqttUsername,
+      required this.mqttPassword,
+      required this.mqttAddress,
+      required this.mqttPort,
+      required this.user,
+      required this.valid});
+
+  factory _$_AppSettingsCls.fromJson(Map<String, dynamic> json) =>
+      _$$_AppSettingsClsFromJson(json);
+
+  @override
+  final String mqttUsername;
+  @override
+  final String mqttPassword;
+  @override
+  final String mqttAddress;
+  @override
+  final int mqttPort;
+  @override
+  final User user;
+  @override
+  final bool valid;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'AppSettingsCls(mqttUsername: $mqttUsername, mqttPassword: $mqttPassword, mqttAddress: $mqttAddress, mqttPort: $mqttPort, user: $user, valid: $valid)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'AppSettingsCls'))
+      ..add(DiagnosticsProperty('mqttUsername', mqttUsername))
+      ..add(DiagnosticsProperty('mqttPassword', mqttPassword))
+      ..add(DiagnosticsProperty('mqttAddress', mqttAddress))
+      ..add(DiagnosticsProperty('mqttPort', mqttPort))
+      ..add(DiagnosticsProperty('user', user))
+      ..add(DiagnosticsProperty('valid', valid));
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_AppSettingsCls &&
+            (identical(other.mqttUsername, mqttUsername) ||
+                other.mqttUsername == mqttUsername) &&
+            (identical(other.mqttPassword, mqttPassword) ||
+                other.mqttPassword == mqttPassword) &&
+            (identical(other.mqttAddress, mqttAddress) ||
+                other.mqttAddress == mqttAddress) &&
+            (identical(other.mqttPort, mqttPort) ||
+                other.mqttPort == mqttPort) &&
+            (identical(other.user, user) || other.user == user) &&
+            (identical(other.valid, valid) || other.valid == valid));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, mqttUsername, mqttPassword,
+      mqttAddress, mqttPort, user, valid);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_AppSettingsClsCopyWith<_$_AppSettingsCls> get copyWith =>
+      __$$_AppSettingsClsCopyWithImpl<_$_AppSettingsCls>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_AppSettingsClsToJson(
+      this,
+    );
+  }
+}
+
+abstract class _AppSettingsCls implements AppSettingsCls {
+  const factory _AppSettingsCls(
+      {required final String mqttUsername,
+      required final String mqttPassword,
+      required final String mqttAddress,
+      required final int mqttPort,
+      required final User user,
+      required final bool valid}) = _$_AppSettingsCls;
+
+  factory _AppSettingsCls.fromJson(Map<String, dynamic> json) =
+      _$_AppSettingsCls.fromJson;
+
+  @override
+  String get mqttUsername;
+  @override
+  String get mqttPassword;
+  @override
+  String get mqttAddress;
+  @override
+  int get mqttPort;
+  @override
+  User get user;
+  @override
+  bool get valid;
+  @override
+  @JsonKey(ignore: true)
+  _$$_AppSettingsClsCopyWith<_$_AppSettingsCls> get copyWith =>
+      throw _privateConstructorUsedError;
+}

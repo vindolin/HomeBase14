59a7b9ee51c440e94305de4f9cdd9f2bec9da7f7
Thomas Schüßler <v@atomar.de>
2023-03-08 20:00:30 +0100

    build batch

---
+++ build2.bat
@@ -0,0 +1,4 @@
+adb connect %1
+del ..\my_app.apks
+java -jar ../bundletool-all-1.14.0.jar build-apks --connected-device --device-id=%1 --bundle=C:\Flutter\homer\build\app\outputs\bundle\release\app-release.aab --output=..\my_app.apks
+java -jar ../bundletool-all-1.14.0.jar install-apks --device-id=%1 --apks=..\my_app.apks

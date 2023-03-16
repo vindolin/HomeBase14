652a0331b6112a942700416cbe32ce0100c33f1d
Thomas Schüßler <v@atomar.de>
2023-03-16 11:49:01 +0100

    added optimistic argument

--- lib/widgets/mqtt_switch_widget.dart
+++ lib/widgets/mqtt_switch_widget.dart
@@ -3,28 +3,44 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '/models/mqtt_devices.dart';
 import '/models/mqtt_providers.dart';
 
-class MqttSwitchWidget extends ConsumerWidget {
+class MqttSwitchWidget extends ConsumerStatefulWidget {
   final String title;
   final String id;
   final String statTopic;
   final String setTopic;
+  final bool optimistic;
 
   const MqttSwitchWidget({
     required this.title,
     required this.id,
     required this.statTopic,
     required this.setTopic,
+    this.optimistic = false,
     super.key,
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final switchState = ref.watch(mqttMessagesFamProvider(statTopic)) ?? 'OFF';
+  ConsumerState<MqttSwitchWidget> createState() => _MqttSwitchWidgetState();
+}
+
+class _MqttSwitchWidgetState extends ConsumerState<MqttSwitchWidget> {
+  bool? switchState;
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.optimistic && switchState == null || !widget.optimistic) {
+      switchState = ref.watch(mqttMessagesFamProvider(widget.statTopic)) == 'ON' ? true : false;
+    }
 
     return Switch(
-      value: switchState == 'ON',
+      value: switchState ?? false,
       onChanged: (value) {
-        ref.read(mqttProvider.notifier).publish(setTopic, switchState);
+        if (widget.optimistic) {
+          setState(() {
+            switchState = value;
+          });
+        }
+        ref.read(mqttProvider.notifier).publish(widget.setTopic, switchState == false ? 'ON' : 'OFF');
       },
     );
   }

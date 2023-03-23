f9c09eb34f8da64796afec1f54d3728043116747
Thomas Schüßler <v@atomar.de>
2023-03-23 15:40:42 +0100

    test

---
+++ lib/models/secrets.dart
@@ -0,0 +1,30 @@
+const encryptionKey = 'SECRET_USED_FOR_SETTINGS_ENCRYPTION';
+
+String serverAddress = 'flutter.dev';
+
+String doorCamUser = 'SECRET';
+String doorCamPassword = Uri.encodeComponent(r'SECRET');
+String doorCamSnapshotPort = '0815';
+String doorCamVideoPort = '0815';
+
+String gardenCamUser = 'guest';
+String gardenCamPassword = r'SECRET';
+String gardenCamSnapshotPort = '0815';
+String gardenCamVideoPort = '0815';
+
+Map<String, Map<String, String>> camData = {
+  'door': {
+    'snapshotUrl': 'http://SECRET',
+    'videoUrl': 'rtsp://SECRET',
+  },
+  'garden': {
+    'snapshotUrl': 'http://SECRET',
+    'videoUrl': 'rtsp://SECRET',
+  },
+};
+
+const grafanaAdddress = 'https://SECRET/d/000000012/grafana-play-home?orgId=11';
+const influxdbUser = 'SECRET';
+const influxdbPassword = 'SECRET';
+const influxdbAddress = 'http://SECRET/query?pretty=false';
+const influxdbDatabase = 'SECRET';

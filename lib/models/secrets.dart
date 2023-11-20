652c6ac01d81089efe6f27d546a3862c9aacf762
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2023-11-20 10:29:24 +0100

    Update secrets.dart

--- lib/models/secrets.dart
+++ lib/models/secrets.dart
@@ -1,32 +1,49 @@
-/// this file only demonstrates how the real file is structured and what variables are set
+const encryptionKey = 'SECRET=';
 
-const encryptionKey = 'SECRET_USED_FOR_SETTINGS_ENCRYPTION';
+String serverAddress = 'secret.com';
 
-String serverAddress = 'flutter.dev';
-
-String doorCamUser = 'SECRET';
+String doorCamUser = 'guest';
 String doorCamPassword = Uri.encodeComponent(r'SECRET');
-String doorCamSnapshotPort = '0815';
-String doorCamVideoPort = '0815';
+String doorCamSnapshotPort = '0000';
+String doorCamVideoPort = '0000';
 
 String gardenCamUser = 'guest';
 String gardenCamPassword = r'SECRET';
-String gardenCamSnapshotPort = '0815';
-String gardenCamVideoPort = '0815';
+String gardenCamSnapshotPort = '0000';
+String gardenCamVideoPort = '0000';
 
 Map<String, Map<String, String>> camData = {
   'door': {
-    'snapshotUrl': 'http://SECRET',
-    'videoUrl': 'rtsp://SECRET',
+    'snapshotUrl': 'http://secret.com:0000/cam_image_door.jpg',
+    'videoUrl': 'https://$doorCamUser:$doorCamPassword@secret.com:0000/13',
+    'rtspUrlLow': 'rtsp://$doorCamUser:$doorCamPassword@secret.com:0000/livestream/13',
+    'rtspUrlHigh': 'rtsp://$doorCamUser:$doorCamPassword@secret.com:0000/livestream/11',
+    'mjpegUrlLow':
+        'https://$doorCamUser:$doorCamPassword@secret.com:0000/livestream/13?action=play&media=mjpeg&user=$doorCamUser&pwd=$doorCamPassword',
+    'mjpegUrlHigh':
+        'https://$doorCamUser:$doorCamPassword@secret.com:0000/livestream/11?action=play&media=mjpeg&user=$doorCamUser&pwd=$doorCamPassword',
   },
   'garden': {
-    'snapshotUrl': 'http://SECRET',
-    'videoUrl': 'rtsp://SECRET',
+    'snapshotUrl': 'http://secret.com:0000/cam_image_garden.jpg',
+    'rtspUrlLow': 'rtsp://$gardenCamUser:$gardenCamPassword@secret.com:0000/13',
+    'rtspUrlHigh': 'rtsp://$gardenCamUser:$gardenCamPassword@secret.com:0000/11',
+    'mjpegUrlLow':
+        'http://$gardenCamUser:$gardenCamPassword@secret.com:0000/mjpegstream.cgi?-chn=13&-usr=$gardenCamUser&-pwd=$gardenCamPassword',
+    'mjpegUrlHigh':
+        'http://$gardenCamUser:$gardenCamPassword@secret.com:0000/mjpegstream.cgi?-chn=11&-usr=$gardenCamUser&-pwd=$gardenCamPassword',
   },
 };
 
-const grafanaAdddress = 'https://SECRET/d/000000012/grafana-play-home?orgId=11';
-const influxdbUser = 'SECRET';
-const influxdbPassword = 'SECRET';
-const influxdbAddress = 'http://SECRET/query?pretty=false';
-const influxdbDatabase = 'SECRET';
+const doorVideoWebsocketUrl =
+    'https://secret.com:0000/mini.html'; // TODOs add nginx proxy but not used at the moment
+const grafanaAdddress = 'https://secret.com:0000/d/SECRET/homebase?orgId=1&refresh=10s&from=now-12h&to=now';
+
+const influxdbUser = 'app';
+const influxdbPassword = 'WhvjcUC95rVgXwxS5FQbYnaJe49x89nF';
+const influxdbAddress = 'https://$influxdbUser:$influxdbPassword@secret.com:0000/query?pretty=false';
+const influxdbDatabase = 'sensors';
+
+const mqttUsername = 'SECRET';
+const mqttPassword = 'SECRET';
+const mqttAddress = 'secret.com';
+const mqttPort = 0000;

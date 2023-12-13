18a49264af40363c3771a04232df82b64c2bd7e9
Thomas Schüßler <v@atomar.de>
2023-12-13 08:17:03 +0100

    styling

--- lib/pages/irrigator/irrigator_page.dart
+++ lib/pages/irrigator/irrigator_page.dart
@@ -21,78 +21,103 @@ class IrrigatorPage extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Irrigator'),
       ),
-      body: Card(
-        elevation: 8,
-        margin: const EdgeInsets.all(16),
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Coffee bush irrigation', style: Theme.of(context).textTheme.titleLarge),
-              const SizedBox(height: 8),
-              Text('Soil moisture: $soilMoisture'),
-              Text('Set target moisture: ${target != null ? target.toInt() : "???"}'),
-              SliderWidget(
-                value: target,
-                min: 0,
-                max: 50,
-                minColor: Colors.grey,
-                maxColor: Colors.red,
-                divisions: 20,
-                inactiveColor: Colors.grey,
-                secondaryActiveColor: Colors.blue,
-                secondaryTrackValue: soilMoisture.toDouble(),
-                onChangeEnd: (value) {
-                  ref.read(mqttProvider.notifier).publish(
-                        'irrigator/set/targetMoisture',
-                        value.toInt().toString(),
-                      );
-                },
-              ),
-              InfluxChartWidget(
-                measurement: 'irrigator',
-                timeSpan: '12h',
-                groupTime: '10m',
-                numberFormat: '#0',
-                labelFormat: '{value} %',
-                fields: {
-                  'target': {
-                    'name': 'Target moisture',
-                    'color': Colors.blue,
-                    'nameFormat': (value) => 'Aktuell ${value.toStringAsFixed(0)} %',
+      body: SingleChildScrollView(
+        child: Card(
+          elevation: 8,
+          margin: const EdgeInsets.all(16),
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Coffee bush irrigation', style: Theme.of(context).textTheme.titleLarge),
+                const SizedBox(height: 8),
+                Text('Soil moisture: $soilMoisture'),
+                Text('Set target moisture: ${target != null ? target.toInt() : "???"}'),
+                SliderWidget(
+                  value: target,
+                  min: 0,
+                  max: 50,
+                  minColor: Colors.grey,
+                  maxColor: Colors.red,
+                  divisions: 20,
+                  inactiveColor: Colors.grey,
+                  secondaryActiveColor: Colors.blue,
+                  secondaryTrackValue: soilMoisture.toDouble(),
+                  onChangeEnd: (value) {
+                    ref.read(mqttProvider.notifier).publish(
+                          'irrigator/set/targetMoisture',
+                          value.toInt().toString(),
+                        );
                   },
-                  'soil': {
-                    'name': 'Soil moisture',
-                    'color': Colors.red,
-                    'nameFormat': (value) => 'Ziel ${value.toStringAsFixed(0)} %',
+                ),
+                InfluxChartWidget(
+                  measurement: 'irrigator',
+                  timeSpan: '12h',
+                  groupTime: '10m',
+                  numberFormat: '#0',
+                  labelFormat: '{value} %',
+                  fields: {
+                    'target': {
+                      'name': 'Target moisture',
+                      'color': Colors.blue,
+                      'nameFormat': (value) => 'Aktuell ${value.toStringAsFixed(0)} %',
+                    },
+                    'soil': {
+                      'name': 'Soil moisture',
+                      'color': Colors.red,
+                      'nameFormat': (value) => 'Ziel ${value.toStringAsFixed(0)} %',
+                    },
                   },
-                },
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    ref.read(mqttProvider.notifier).publish('irrigator/measure', '');
-                  },
-                  child: const Text('Check now', style: TextStyle(fontSize: 20))),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        ref.read(mqttProvider.notifier).publish('irrigator/openValve', '');
-                      },
-                      child: const Text('Open valve', style: TextStyle(fontSize: 20))),
-                  ElevatedButton(
-                      onPressed: () {
-                        ref.read(mqttProvider.notifier).publish('irrigator/closeValve', '');
-                      },
-                      child: const Text('Close valve', style: TextStyle(fontSize: 20))),
-                ],
-              ),
-            ],
+                ),
+                Card(
+                  color: Colors.grey.shade800,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Actions:',
+                            style: TextStyle(color: Colors.grey.shade300, fontWeight: FontWeight.bold, fontSize: 20)),
+                        const SizedBox(height: 8),
+                        ElevatedButton(
+                            onPressed: () {
+                              ref.read(mqttProvider.notifier).publish('irrigator/measure', '');
+                            },
+                            child: const Padding(
+                              padding: EdgeInsets.all(4.0),
+                              child: Text('Check now', style: TextStyle(fontSize: 18)),
+                            )),
+                        const SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ElevatedButton(
+                                onPressed: () {
+                                  ref.read(mqttProvider.notifier).publish('irrigator/openValve', '');
+                                },
+                                child: const Padding(
+                                  padding: EdgeInsets.all(4.0),
+                                  child: Text('Open valve', style: TextStyle(fontSize: 18)),
+                                )),
+                            ElevatedButton(
+                                onPressed: () {
+                                  ref.read(mqttProvider.notifier).publish('irrigator/closeValve', '');
+                                },
+                                child: const Padding(
+                                  padding: EdgeInsets.all(4.0),
+                                  child: Text('Close valve', style: TextStyle(fontSize: 18)),
+                                )),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),

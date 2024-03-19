d36ce418010e37e9749c8ddc394cbdcd9b6b5227
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-03-19 07:32:04 +0100

    mona page

---
+++ lib/pages/mona/mona_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/material.dart';
+
+// import '/models/mqtt_providers.dart';
+import '/styles/styles.dart';
+import '/utils.dart';
+import '/widgets/connection_bar_widget.dart';
+
+/// Random stuff for Mona
+class MonaPage extends ConsumerWidget {
+  const MonaPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    log('MonaPage.build()');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mona'),
+        actions: const [ConnectionBar()],
+      ),
+      body: Container(
+        decoration: fancyBackground,
+        child: GridView.count(
+          padding: const EdgeInsets.all(8),
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          children: const [
+            Card(),
+          ],
+        ),
+      ),
+    );
+  }
+}

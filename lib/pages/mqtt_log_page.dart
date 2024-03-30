bbac690ce077a67b0792793a100574b2d732d2bb
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-03-30 19:47:45 +0100

    touch to pause

--- lib/pages/mqtt_log_page.dart
+++ lib/pages/mqtt_log_page.dart
@@ -7,13 +7,22 @@ import '/models/mqtt_providers.dart';
 
 final messageQueue = Queue();
 
-class MqttLog extends ConsumerWidget {
+class MqttLog extends ConsumerStatefulWidget {
   const MqttLog({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<MqttLog> createState() => _MqttLogState();
+}
+
+class _MqttLogState extends ConsumerState<MqttLog> {
+  bool paused = false;
+
+  @override
+  Widget build(BuildContext context) {
     ref.watch(messageProvider).when(
           data: (data) {
+            if (paused) return null;
+
             if (messageQueue.length > 100) {
               messageQueue.removeFirst();
             }
@@ -25,34 +34,41 @@ class MqttLog extends ConsumerWidget {
         );
 
     // return Text of last 100 messages
-    return ListView.builder(
-      itemCount: messageQueue.length,
-      itemBuilder: (context, index) {
-        // newest message first
-        int reverseIndex = messageQueue.length - 1 - index;
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '${messageQueue.elementAt(reverseIndex)?.topic}',
-              style: const TextStyle(
-                fontSize: 16,
-                color: Colors.green,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Text(
-                '${messageQueue.elementAt(reverseIndex)?.payload}',
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          paused = !paused;
+        });
+      },
+      child: ListView.builder(
+        itemCount: messageQueue.length,
+        itemBuilder: (context, index) {
+          // newest message first
+          int reverseIndex = messageQueue.length - 1 - index;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${messageQueue.elementAt(reverseIndex).topic}',
                 style: const TextStyle(
-                  fontSize: 12,
-                  color: Colors.greenAccent,
+                  fontSize: 16,
+                  color: Colors.green,
                 ),
               ),
-            ),
-          ],
-        );
-      },
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Text(
+                  '${messageQueue.elementAt(reverseIndex).payload}',
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.greenAccent,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }

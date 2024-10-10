b9ccd2a50f8139e44f95f36834df23c6c2717e33
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-10-10 14:35:08 +0200

    replace encrypted secrets in file

--- lib/encrypt_secrets.dart
+++ lib/encrypt_secrets.dart
@@ -1,19 +1,33 @@
 // ignore_for_file: avoid_print
-// import 'dart:convert';
+// import 'dart:io';
+import 'dart:convert';
+import 'dart:io';
 
-// import '/models/secrets_new.dart' as secrets; // not in repo
+import '/models/secrets_new.dart' as secrets; // not in repo
 import '/models/encryption.dart' as encryption;
 
-const certKey = '''-----BEGIN RSA PRIVATE KEY-----
-
-insert mqtt cert key here
-
------END RSA PRIVATE KEY-----''';
-
 void main(List<String> args) {
+  // File('..\\mqtt_cert_key.txt').readAsString().then((String certKey) {
+  //   print(certKey);
+  // });
+  // const payload = certKey;
+
   final key = args[0];
-  const payload = certKey;
-  // String payload = jsonEncode(secrets.secretsMap);
+  String payload = jsonEncode(secrets.secretsMap);
   final encrypted = encryption.encrypt(key, payload);
-  print(encrypted);
+  // print(encrypted);
+
+  final filePath = 'lib/models/secrets_provider.dart';
+  final file = File(filePath);
+
+  // open the file secrets_provider.dart and replace the _encryptedSecrets with the output of this script
+  file.readAsString().then(
+    (String contents) {
+      final newContents = contents.replaceFirst(
+        RegExp(r"(?<=final _encryptedSecrets \=)\n\s{6}r\'(.+)\';$", multiLine: true),
+        '\n      r\'$encrypted\';',
+      );
+      file.writeAsString(newContents);
+    },
+  );
 }

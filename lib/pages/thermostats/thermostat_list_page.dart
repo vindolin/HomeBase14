4ae616ef177c8d1120b091b9b11e5ac3c358e013
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-10-16 15:57:54 +0200

    oops, forgot shortcut for thermostats

--- lib/pages/thermostats/thermostat_list_page.dart
+++ lib/pages/thermostats/thermostat_list_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_translate/flutter_translate.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
+import '/shortcut_wrapper.dart';
 import '/utils.dart';
 import '/ui_helpers.dart';
 import '/models/mqtt_devices.dart';
@@ -47,102 +48,105 @@ class ThermostatListPage extends ConsumerWidget {
     final currentHeatingSetpointAvg =
         thermostatDevices.values.averageBy((thermostatDevice) => thermostatDevice.currentHeatingSetpoint);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Text(translate('device_names.thermostats')),
-            const Spacer(),
-            Text(
-              '⌀${localTemperatureAvg.toStringAsFixed(1)}°C',
-              style: const TextStyle().copyWith(
-                fontWeight: FontWeight.w700,
-                color: getTemperatureColor(localTemperatureAvg, currentHeatingSetpointAvg),
-              ),
-            )
-          ],
+    return shortcutWrapper(
+      context,
+      Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Text(translate('device_names.thermostats')),
+              const Spacer(),
+              Text(
+                '⌀${localTemperatureAvg.toStringAsFixed(1)}°C',
+                style: const TextStyle().copyWith(
+                  fontWeight: FontWeight.w700,
+                  color: getTemperatureColor(localTemperatureAvg, currentHeatingSetpointAvg),
+                ),
+              )
+            ],
+          ),
+          actions: const [ConnectionBar()],
         ),
-        actions: const [ConnectionBar()],
-      ),
-      body: GridView.count(
-        childAspectRatio: 2.1,
-        crossAxisCount: 2,
-        crossAxisSpacing: 4,
-        mainAxisSpacing: 4,
-        padding: const EdgeInsets.all(4),
-        children: thermostatDevices.entries.map(
-          (entry) {
-            final key = entry.key;
-            final device = entry.value;
+        body: GridView.count(
+          childAspectRatio: 2.1,
+          crossAxisCount: 2,
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+          padding: const EdgeInsets.all(4),
+          children: thermostatDevices.entries.map(
+            (entry) {
+              final key = entry.key;
+              final device = entry.value;
 
-            final tempColor = getTemperatureColor(
-              device.localTemperature,
-              device.currentHeatingSetpoint.toDouble(),
-            );
-            return Card(
-              clipBehavior: Clip.antiAlias,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              child: InkWell(
-                onTap: () {
-                  modalDialog(
-                    context,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(deviceNames[key]!, style: Theme.of(context).textTheme.titleLarge),
-                        const SizedBox(height: 16),
-                        ThermostatInput(
-                          deviceId: device.deviceId,
-                          closeAction: () => Navigator.of(context).pop(),
-                        ),
-                      ],
-                    ),
-                    constraints: const BoxConstraints(maxHeight: 300),
-                  );
-                },
-                child: Row(
-                  children: [
-                    device.localTemperature < device.currentHeatingSetpoint
-                        ? PulsatingIcon(
-                            iconData: Icons.thermostat,
-                            color: tempColor!,
-                            size: 36,
-                            lowerBound: 0.1,
-                            initialValue: (1.0 - 0.1) *
-                                Random()
-                                    .nextDouble(), // randomize the initial value to avoid all icons pulsating in sync
-                          )
-                        : Icon(
-                            Icons.thermostat,
-                            color: tempColor!,
-                            size: 36,
-                          ),
-                    Expanded(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
+              final tempColor = getTemperatureColor(
+                device.localTemperature,
+                device.currentHeatingSetpoint.toDouble(),
+              );
+              return Card(
+                clipBehavior: Clip.antiAlias,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    modalDialog(
+                      context,
+                      Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            deviceNames[key]!,
-                            style: Theme.of(context).textTheme.titleMedium,
-                            overflow: TextOverflow.fade,
-                            softWrap: true,
-                          ),
-                          ThermostatReadings(
-                            currentHeatingSetpoint: device.currentHeatingSetpoint,
-                            localTemperature: device.localTemperature,
+                          Text(deviceNames[key]!, style: Theme.of(context).textTheme.titleLarge),
+                          const SizedBox(height: 16),
+                          ThermostatInput(
+                            deviceId: device.deviceId,
+                            closeAction: () => Navigator.of(context).pop(),
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      constraints: const BoxConstraints(maxHeight: 300),
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      device.localTemperature < device.currentHeatingSetpoint
+                          ? PulsatingIcon(
+                              iconData: Icons.thermostat,
+                              color: tempColor!,
+                              size: 36,
+                              lowerBound: 0.1,
+                              initialValue: (1.0 - 0.1) *
+                                  Random()
+                                      .nextDouble(), // randomize the initial value to avoid all icons pulsating in sync
+                            )
+                          : Icon(
+                              Icons.thermostat,
+                              color: tempColor!,
+                              size: 36,
+                            ),
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              deviceNames[key]!,
+                              style: Theme.of(context).textTheme.titleMedium,
+                              overflow: TextOverflow.fade,
+                              softWrap: true,
+                            ),
+                            ThermostatReadings(
+                              currentHeatingSetpoint: device.currentHeatingSetpoint,
+                              localTemperature: device.localTemperature,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
-        ).toList(),
+              );
+            },
+          ).toList(),
+        ),
       ),
     );
   }

d034aa374678c25432c19880939e2bad3b15c344
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2024-12-14 17:13:54 +0100

    simplify network_addresses, add logger

--- lib/models/network_addresses_provider.dart
+++ lib/models/network_addresses_provider.dart
@@ -1,12 +1,22 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-// import '/utils.dart';
 import '/models/secrets_provider.dart';
+
+import '/util/logger.dart';
 
 part 'network_addresses_provider.g.dart';
 
-// const doorVideoWebsocketUrl =
-//     'https://$serverAddress:3001/mini.html'; // TODOs add nginx proxy but not used at the moment
+extension StringExtension on String {
+  String capitalize() {
+    if (isEmpty) return this;
+    return this[0].toUpperCase() + substring(1);
+  }
+}
+
+Map<String, String> resolutions = {
+  'low': '13',
+  'medium': '12',
+  'high': '11',
+};
 
 @Riverpod(keepAlive: true)
 class NetworkAddresses extends _$NetworkAddresses {
@@ -21,51 +31,41 @@ class NetworkAddresses extends _$NetworkAddresses {
     final snapshotPort = secrets['general']['snapshotPort'];
     final grafanaProjectHash = secrets['general']['grafanaProjectHash'];
 
-    final doorCamVideoPort = secrets['network']['doorCamVideoPort'];
-    final gardenCamVideoPort = secrets['network']['gardenCamVideoPort'];
+    final cameraIds = {
+      'door': 'Door',
+      'garden': 'Garden',
+    };
 
-    final doorCamUser = secrets['general']['doorCamUser'];
-    final doorCamPassword = secrets['general']['doorCamPassword'];
-    final doorCamVideoAddress = secrets['network']['doorCamVideoAddress'];
-    final doorCamSnapshotPort = secrets['general']['doorCamSnapshotPort'];
+    final cameras = Map.fromEntries(cameraIds.keys.map((id) {
+      final camUser = secrets['general']['${id}CamUser'];
+      final camPassword = secrets['general']['${id}CamPassword'];
+      final camVideoAddress = secrets['network']['${id}CamVideoAddress'];
+      final camSnapshotPort = secrets['general']['${id}CamSnapshotPort'];
+      final camVideoPort = secrets['network']['${id}CamVideoPort'];
 
-    final gardenCamUser = secrets['general']['gardenCamUser'];
-    final gardenCamPassword = secrets['general']['gardenCamPassword'];
-    final gardenCamVideoAddress = secrets['network']['gardenCamVideoAddress'];
-    final gardenCamSnapshotPort = secrets['general']['gardenCamSnapshotPort'];
+      final map = {
+        'snapshotUrl': 'http://$serverAddress:$snapshotPort/cam_image_$id.jpg',
+      };
+
+      resolutions.forEach((key, value) {
+        final name = key.capitalize();
+        map['videoStreamUrl$name'] = 'rtsp://$camUser:$camPassword@$camVideoAddress:$camVideoPort/livestream/$value';
+
+        map['mjpegUrl$name'] =
+            'https://$camUser:$camPassword@$camVideoAddress:$camSnapshotPort/livestream/$value?action=play&media=mjpeg&user=$camUser&pwd=$camPassword';
+      });
+
+      return MapEntry(id, map);
+    }));
 
     final networkAddresses = {
       'grafana': 'https://$grafanaAddress/d/$grafanaProjectHash/homebase?orgId=1&refresh=10s&from=now-12h&to=now',
       'influxdb': 'https://$influxdbUser:$influxdbPassword@$serverAddress:8085/query?pretty=false',
-      'allCameraIds': {
-        'door': 'Door',
-        'garden': 'Garden',
-      },
-      'door': {
-        'snapshotUrl': 'http://$serverAddress:$snapshotPort/cam_image_door.jpg',
-        'mjpegUrlLow':
-            'https://$doorCamUser:$doorCamPassword@$doorCamVideoAddress:$doorCamSnapshotPort/livestream/13?action=play&media=mjpeg&user=$doorCamUser&pwd=$doorCamPassword',
-        'mjpegUrlHigh':
-            'https://$doorCamUser:$doorCamPassword@$doorCamVideoAddress:$doorCamSnapshotPort/livestream/11?action=play&media=mjpeg&user=$doorCamUser&pwd=$doorCamPassword',
-        'videoStreamUrlLow':
-            'rtsp://$doorCamUser:$doorCamPassword@$doorCamVideoAddress:$doorCamVideoPort/livestream/13',
-        'videoStreamUrlHigh':
-            'rtsp://$doorCamUser:$doorCamPassword@$doorCamVideoAddress:$doorCamVideoPort/livestream/12',
-      },
-      'garden': {
-        'snapshotUrl': 'http://$serverAddress:$snapshotPort/cam_image_garden.jpg',
-        'mjpegUrlLow':
-            'https://$gardenCamUser:$gardenCamPassword@$gardenCamVideoAddress:$gardenCamSnapshotPort/livestream/13?action=play&media=mjpeg&user=$gardenCamUser&pwd=$gardenCamPassword',
-        'mjpegUrlHigh':
-            'https://$gardenCamUser:$gardenCamPassword@$gardenCamVideoAddress:$gardenCamSnapshotPort/livestream/11?action=play&media=mjpeg&user=$gardenCamUser&pwd=$gardenCamPassword',
-        'videoStreamUrlLow':
-            'rtsp://$gardenCamUser:$gardenCamPassword@$gardenCamVideoAddress:$gardenCamVideoPort/livestream/13',
-        'videoStreamUrlHigh':
-            'rtsp://$gardenCamUser:$gardenCamPassword@$gardenCamVideoAddress:$gardenCamVideoPort/livestream/12',
-      },
+      'allCameraIds': cameraIds,
+      ...cameras,
     };
 
-    // log(networkAddresses);
+    logger.i(networkAddresses);
     return networkAddresses;
   }
 }
---
+++ lib/util/logger.dart
@@ -0,0 +1,14 @@
+import 'package:logger/logger.dart';
+
+var logger = Logger(
+  printer: PrettyPrinter(
+    methodCount: 1, // Number of method calls to be displayed
+    errorMethodCount: 8, // Number of method calls if stacktrace is provided
+    lineLength: 120, // Width of the output
+    colors: true, // Colorful log messages
+    printEmojis: true, // Print an emoji for each log message
+    // Should each log print contain a timestamp
+    // dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart,
+    dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart,
+  ),
+);
